5bdb0b131e13f54fb6fa4f453b0cf457465b9e83
swapnil <swapnil130501@gmail.com>
2023-05-29 14:03:42 +0530

    added getUserId in repository

--- src/index.js
+++ src/index.js
@@ -4,16 +4,22 @@ const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/index');
 const { PORT } = require('./config/serverConfig');
 
+// const UserRepository = require('./repository/user-repository');
+
 const app = express();
 
 const prepareAndStartServer = () => {
-    
-    app.listen(PORT, () => {
+
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
-
         app.use('/api', apiRoutes);
+    
+    app.listen(PORT, async () => {
         console.log(`Server started on Port: ${PORT}`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
+        
     })
 }
 prepareAndStartServer();
